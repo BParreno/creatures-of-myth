c5eae8158ebcedade393f975f7b5620ba568bc9c
BParreno <biparreno@sudamericano.edu.ec>
2024-10-23 19:47:13 -0500

    Commit 4 CRUD
    (create y update no modificados debido a error en "data")

--- src/myth-creatures/dto/create-myth-creature.dto.ts
+++ src/myth-creatures/dto/create-myth-creature.dto.ts
@@ -1,1 +1,39 @@
-export class CreateMythCreatureDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+// Definición del DTO para Skill
+export class SkillDto {
+  @ApiProperty()
+  name: string;
+  @ApiProperty()
+  description: string;
+}
+
+// Definición del DTO para Weakness
+export class WeaknessDto {
+  @ApiProperty()
+  name: string;
+  @ApiProperty()
+  description: string;
+}
+
+// DTO principal para Myth Creatures, incluyendo Skill y Weakness
+export class CreateMythCreatureDto {
+  @ApiProperty()
+  name: string;
+  @ApiProperty()
+  description: string;
+  @ApiProperty()
+  origin: string;
+
+  @ApiProperty({
+    type: [SkillDto],
+    description: 'List of skills for the creature',
+  })
+  skills: SkillDto[];
+
+  @ApiProperty({
+    type: [WeaknessDto],
+    description: 'List of weaknesses for the creature',
+  })
+  weaknesses: WeaknessDto[];
+}
